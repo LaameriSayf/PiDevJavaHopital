c47be8887a58dc82bcaf84c993e4743f70face12
Slouumaa <abdessalem.chaouch@esprit.tn>
2024-05-01 12:56:26 +0100

    Détail

--- JavaPiHopital/src/main/java/Controller/AfficherController.java
+++ JavaPiHopital/src/main/java/Controller/AfficherController.java
@@ -2,109 +2,128 @@ package Controller;
 
 import Model.Medicament;
 import Service.MedicamentService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+
+import javafx.scene.control.Button;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.util.Callback;
 
 import java.io.File;
+
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class AfficherController implements Initializable {
+    @FXML
+    private ImageView imageId;
 
     @FXML
-    private ListView<Medicament> frontWost;
+    private Text txt1;
+
+    @FXML
+    private Text txt2;
+
+    @FXML
+    private Text txt3;
+
+    @FXML
+    private Text txt4;
+
+    @FXML
+    private AnchorPane DetailForm;
+
+
+    @FXML
+    private AnchorPane ListMedicamentForm;
+
+    @FXML
+    private GridPane GridMedicament;
+
+
+    private static final int NUM_COLUMNS = 3;
 
     private final MedicamentService medicamentService = new MedicamentService();
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        List<Medicament> medicaments = medicamentService.getData();
-        frontWost.getItems().addAll(medicaments);
-        frontWost.setCellFactory(new Callback<ListView<Medicament>, ListCell<Medicament>>() {
-            @Override
-            public ListCell<Medicament> call(ListView<Medicament> param) {
-                return new ListCell<Medicament>() {
-                    @Override
-                    protected void updateItem(Medicament medicament, boolean empty) {
-                        super.updateItem(medicament, empty);
+        List<Medicament> listeMedicaments = medicamentService.getData();
 
-                        if (empty || medicament == null) {
-                            setText(null);
-                            setGraphic(null);
-                        } else {
-                            int index = getIndex();
-                            if (index % 3 == 0 && index + 2 < frontWost.getItems().size()) { // Nouvelle ligne avec au moins 3 éléments restants
-                                HBox hbox = new HBox();
-                                hbox.setSpacing(20); // Espacement entre les éléments
-                                hbox.setStyle("-fx-padding: 10;" + // Espacement intérieur
-                                        "-fx-border-style: solid inside;" +
-                                        "-fx-border-width: 1;" +
-                                        "-fx-border-insets: 5;" +
-                                        "-fx-border-radius: 5;" +
-                                        "-fx-border-color: black;"); // Style du cadre
+        int row = 0;
+        int col = 0;
 
-                                for (int i = 0; i < 3; i++) {
-                                    Medicament currentMedicament = frontWost.getItems().get(index + i);
-                                    VBox vbox = createVBox(currentMedicament);
-                                    hbox.getChildren().add(vbox);
-                                }
-                                setGraphic(hbox);
-                            } else {
-                                // Si ce n'est pas le début d'une nouvelle ligne ou si le nombre d'éléments restants est insuffisant, ne rien afficher
-                                setGraphic(null);
-                            }
-                        }
-                    }
-                };
+        // Centrer les éléments dans le GridPane
+        GridMedicament.setAlignment(Pos.CENTER);
+        GridMedicament.setHgap(10); // Ajouter un espacement horizontal entre les éléments
+        GridMedicament.setVgap(10); // Ajouter un espacement vertical entre les éléments
+
+        for (Medicament medicament : listeMedicaments) {
+            // Créer une HBox pour contenir l'image et le bouton "Voir plus"
+            HBox hbox = new HBox();
+            hbox.setAlignment(Pos.CENTER);
+            hbox.setSpacing(5); // Ajouter un espacement entre l'image et le bouton
+            hbox.setPadding(new Insets(5)); // Ajouter des marges autour de la HBox
+
+            // Créez une ImageView pour afficher l'image
+            ImageView imageView = new ImageView(new Image(new File(medicament.getImage()).toURI().toString()));
+            imageView.setFitWidth(120); // Ajustez la taille selon vos besoins
+            imageView.setFitHeight(120);
+
+            // Créez le bouton "Voir plus"
+            Button voirPlusButton = new Button("Voir plus");
+
+            // Configuration de l'action du bouton "Voir plus"
+            configureVoirPlusButton(voirPlusButton, medicament);
+
+            // Ajoutez l'ImageView et le bouton "Voir plus" à la HBox
+            hbox.getChildren().addAll(imageView, voirPlusButton);
+
+            // Ajoutez la HBox à la grille
+            GridMedicament.add(hbox, col, row);
+
+            // Incrémentez les indices de colonne
+            col++;
+
+            // Passez à la ligne suivante s'il y a trop d'éléments dans la même ligne
+            if (col == NUM_COLUMNS) {
+                col = 0;
+                row++; // Passez à la ligne suivante
             }
+        }
+    }
+
+    private void configureVoirPlusButton(Button voirPlusButton, Medicament medicament) {
+        voirPlusButton.setOnAction(event -> {
+            afficherDetailsMedicament(medicament);
         });
     }
 
-    private VBox createVBox(Medicament medicament) {
-        VBox vbox = new VBox();
-        vbox.setSpacing(5); // Espacement entre les éléments
-        vbox.setAlignment(Pos.CENTER); // Centrage vertical
-        vbox.setBorder(new Border(new BorderStroke(Color.BLACK,
-                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT))); // Ajout du cadre
-
-        ImageView imageView = createImageView(medicament);
-        Text nameText = new Text(medicament.getNom_med());
-        nameText.setFont(Font.font("Times New Roman", FontWeight.BOLD, 12)); // Police Times New Roman, gras, taille 12
-        nameText.setTextAlignment(TextAlignment.CENTER); // Centrage horizontal du nom
-        Text referenceText = new Text(medicament.getRef_med());
-        referenceText.setFont(Font.font("Times New Roman", 12)); // Police Times New Roman, taille 12
-        referenceText.setTextAlignment(TextAlignment.CENTER); // Centrage horizontal de la référence
-
-        vbox.getChildren().addAll(imageView, nameText, referenceText);
-
-        return vbox;
+    private void afficherDetailsMedicament(Medicament medicament) {
+        // Mettre à jour les éléments visuels avec les détails du médicament
+        imageId.setImage(new Image(new File(medicament.getImage()).toURI().toString()));
+        txt1.setText(medicament.getNom_med());
+        txt3.setText(medicament.getRef_med());
+        txt2.setText(medicament.getEtat());
+        txt4.setText(medicament.getCategorie().getNom_cat());
+        DetailForm.setVisible(true);
+        ListMedicamentForm.setVisible(false);
     }
 
-    private ImageView createImageView(Medicament medicament) {
-        String imagePath = medicament.getImage(); // Supposons que vous avez un chemin d'image absolu ici
-        ImageView imageView = new ImageView();
-        imageView.setFitWidth(200);
-        imageView.setFitHeight(200);
-
-        if (imagePath != null) {
-            File file = new File(imagePath);
-            Image image = new Image(file.toURI().toString());
-            imageView.setImage(image);
-        }
-
-        return imageView;
+    @FXML
+    void RetourBtn(ActionEvent event) {
+        DetailForm.setVisible(false);
+        ListMedicamentForm.setVisible(true);
     }
+
+
 }
